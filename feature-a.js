e208db9179354a3c5133844c6dd893b5a7ba5a35
elorm116 <aezottor@gmail.com>
2025-01-31 11:31:31 +0000

    added a new feature

---
+++ feature-a.js
@@ -0,0 +1,9 @@
+// Display an alert when the page loads
+window.onload = function() {
+    alert("Welcome to my website!");
+};
+
+// Function to change text when a button is clicked
+function changeText() {
+    document.getElementById("message").innerHTML = "You clicked the button!";
+}
